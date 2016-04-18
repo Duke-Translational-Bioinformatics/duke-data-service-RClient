7fc0ef0eb91c50fcfcaa69dd2c1ceb7a969fd1ba
benneely <nigelneely@gmail.com>
2016-04-18 13:52:42 -0400

    modified login procedure to match python client

--- R/login.R
+++ R/login.R
@@ -27,14 +27,12 @@ ddslogin <- function(url=NA, rememberMe=TRUE) {
     #nothing needs to be done, we have valid token and it hasn't expired
   } else if ((.getCache('sa_api_token') != "") & (as.numeric(.getCache('sa_api_token_expires')) < as.integer(as.POSIXct( Sys.time() )))) {
     #refreshing our sa_api_token
-    .getSaApiToken()
+    .getAndSetToken(.getCache('url'));
   } else {
-    .setCache('url_api_token',.scrapesessioncookie(.getCache('url')));
-    .getUserInformation(api_token=.getCache('url_api_token'));
-    #Since we don't have a sa_api_token and this is the preferred connection method, we'll step through
-    #that workflow to obtain a sa_api_token
-    #Let's add this information to our cache key 'curlHeader'
-    .getSaApiToken(url_api_token=.getCache('url_api_token'))
+    ##reworking this workflow to align with the python client
+    .getAndSetKeys(.getCache('url'));
+    .getAndSetToken();
+    .getUserInformation();
   }
   #We're now "logged in", if rememberMe is true, we need to write out to the config file
   #################################################################################################
@@ -71,63 +69,28 @@ ddslogout <- function(){
   .setCache('userid',r$body$id)
 }
 
-.getSaApiToken <- function(url_api_token=.getCache('sa_api_token')){
-  #Check if there is already a user key if not generate one
-  #########################################################
-  if (.getCache('user_key')=="") {
-    #first check if the system has one, could be that someone created one for python client, don't want to mess that up!
-    r = ddsRequest(endpoint='/current_user/api_key',
-                   httpheader=c(.getCache('curlHeader'),'Authorization'=url_api_token))
-    if (r$status=="404") {
-      r = ddsRequest(customrequest="PUT",
-                     endpoint='/current_user/api_key',
-                     httpheader=c(.getCache('curlHeader'),'Authorization'=url_api_token))
-      .setCache('user_key',r$body$key)
-    } else {
-      .setCache('user_key',r$body$key)
-    }
-  }
-  else {
-    #maybe always want to check the server and not trust the config version?
-  }
-  if (.getCache('sa_key')=="") {
-    body = list(name='ddsRClient',
-                description='R programmatic Client',
-                repo_url='https://github.com/Duke-Translational-Bioinformatics/duke-data-service-RClient')
-    r = ddsRequest(customrequest="POST",
-                   endpoint='/software_agents',
-                   body_list=body,
-                   httpheader=c(.getCache('curlHeader'),'Authorization'=url_api_token))
-    if (r$status==201) {
-      .setCache('sa_id',r$body$id)
-      r = ddsRequest(customrequest="GET",
-                     endpoint=paste0('/software_agents/',.getCache('sa_id'),'/api_key'),
-                     httpheader=c(.getCache('curlHeader'),'Authorization'=url_api_token))
-      if (r$status==200) {
-        .setCache('sa_key',r$body$key)
-      }
-    }
-  }
-  #finally get the sa api token
-  body = list(agent_key=.getCache('sa_key'),
-              user_key=.getCache('user_key'))
+.getAndSetToken <- function(user_key=.getCache('user_key'),sa_key=.getCache("sa_key")){
+  body = list(agent_key=sa_key,
+              user_key=user_key)
   r = ddsRequest(customrequest="POST",
                  endpoint='/software_agents/api_token',
                  body_list=body)
   .setCache('sa_api_token',r$body$api_token)
   .setCache('sa_api_token_expires',r$body$expires_on)
+
 }
 
-
-.scrapesessioncookie <- function(endpoint) {
+.getAndSetKeys <- function(endpoint) {
   msg1<-paste0(c("You don't have a software agent key cached.\n",
                  "This is an important token when working with programmtic clients.\n",
                  "I'm going to redirect you to the DDS portal, please login and then\n",
-                 "open the developer tools to copy the cookie 'apiToken'.\n",
+                 "provide the requested information. If you need additional assistance,\n",
+                 "please use these instructions:\n",
+                 "https://github.com/Duke-GCB/DukeDSClient/blob/master/docs/GettingAgentAndUserKeys.md \n",
                  "Press Return to continue>>>>"),
                collapse="")
   ack <-readline(writeLines(msg1))
-  browseURL(endpoint);
-  url_api_token = readline("Paste the apiToken cookie here: ");
-  return(url_api_token)
+  browseURL(endpoint)
+  .setCache('sa_key',readline("Paste the Agent Secret Key: "))
+  .setCache('user_key', readline("Paste the User Key: "))
 }
