f11d5ca3583bc18888a1cb1d8d221105c2be9b77
benneely <nigelneely@gmail.com>
2016-05-25 15:46:34 -0400

    download functionality added

--- R/ddsDownload.R
+++ R/ddsDownload.R
@@ -1,13 +1,15 @@
-#' DDS RESTful File Upload.
+#' DDS RESTful File Download.
 #'
 #' @param local_folder The location (UNC path) of a local folder where files will be saved
 #' @param project The DDS project to download.
+#' @param stop_on_mismatch=TRUE A logical indicating if ddsDownload should abort due to mistached DDS hash and current download
 #' @param chunk_size_bytes optionally, files can be downloaded per chunck, specify download chunk size here.
 #' @return Standard Output indicating a successful or failed download.
 #' @examples
-#' ddsUpload(local_folder="/Users/nn/Desktop/upload_testers",project="UploadTester")
+#' ddsDownload(local_folder="/Users/nn31/Desktop/upload_testers",project="UploadTester")
 ddsDownload <- function(project=NULL,
                         local_folder=NULL,
+                        stop_on_mismatch=TRUE,
                         chunk_size_bytes = 1293892) {
   if (is.null(local_folder) | is.null(project))
     stop(sprintf('ddsDownload requires valid input for local_folder and/or project'))
@@ -64,6 +66,7 @@ ddsDownload <- function(project=NULL,
   if (length(folders_to_add)>0) {
     null = lapply(folders_to_add, function(x) dir.create(x))
     transaction@local$dirs = c(transaction@local$dirs,folders_to_add)
+    transaction@local$dirs = transaction@local$dirs[transaction@local$dirs != ""]
   }
   ################################################
   #Now all local folders are on DDS, let's put
@@ -73,49 +76,44 @@ ddsDownload <- function(project=NULL,
   files_to_add = setdiff(transaction@dds$filenames,transaction@local$filenames)
   if (length(files_to_add)>0) {
     for (i in 1:length(files_to_add)) {
-      if (dirname(files_to_add[i])==".") {folderName=NULL}
-      else {
-        if (transaction@dds$dirs[dirname(files_to_add[i])==transaction@dds$dirs]==dirname(files_to_add[i])) {
-          folderName = dirname(files_to_add[i])
-        }
-      }
+      # if (dirname(files_to_add[i])==".") {folderName=NULL}
+      # else {
+      #   if (transaction@dds$dirs[dirname(files_to_add[i])==transaction@dds$dirs]==dirname(files_to_add[i])) {
+      #     folderName = dirname(files_to_add[i])
+      #   }
+      # }
       .downloadFile(filepath = files_to_add[i],
-                    fileid = transaction@dds$fileids[files_to_add[i]==transaction@dds$filenames]
+                    fileid = transaction@dds$fileids[files_to_add[i]==transaction@dds$filenames],
                     file_hash = names(transaction@dds$filenames[transaction@dds$filenames==files_to_add[i]]),
-                    folder_loc = folderName,
+                    stopom=stop_on_mismatch,
                     chunksizeBytes=chunk_size_bytes)
     }
   }
 
 }
 
-.downloadfile <- function(filepath,
+.downloadFile <- function(filepath,
                           fileid,
                           file_hash,
-                          folder_loc,
+                          stopom=stop_on_mismatch,
                           chunksizeBytes){
+  message(sprintf('Downloading file %s:',filepath))
   #Let's get the swift URL of the information
   #At some point, let's rewrite
   #http://stackoverflow.com/questions/17306695/httr-get-function-running-out-of-space-when-downloading-a-large-file
   r = ddsRequest(customrequest="GET",
                  endpoint=paste0('/files/',fileid,'/url'))
   file_url = paste0(r$body$host,r$body$url)
-  if (is.null(folder_loc)){
-    download.file(url = file_url,
-                  destfile = filename,
-                  mode='wb',
-                  quiet=TRUE,
-                  extra = paste0("--header 'Authorization:", .getCache('sa_api_token'),"'"))
-  } else {
-    download.file(url = file_url,
-                  destfile = filename,
-                  mode='wb',
-                  quiet=TRUE,
-                  extra = paste0("--header 'Authorization:", .getCache('sa_api_token'),"'"))
-
-  }
-  #roadmap
-  #download.file
+  host = r$body$host
+  endpt = r$body$url
+  #let's plan to update this at some point to be more robust - see above url for tip
+  download.file(url = file_url,
+                  destfile = filepath,
+                  mode='wb')
   #see if hash matches dds, if not, throw a warning providing each hash
+  if (!digest(filepath,algo='md5',file=TRUE)==file_hash) {
+    if (stopom) {stop(sprintf("Downloaded File (%s) from DDS does not match the DDS reported Hash. If DDS doesn't have a hash, rerun ddsDownload with the stop_on_mismatch=FALSE."))}
+    message(sprintf("NOTE: File %s, has a mismatched hash on disk when compared to DDS, the stop_on_mismatch flag is TRUE, so continuing to download to local disk."))
+  }
   #throw a warning before overwriting a file, instruct user to change the paramerter to fileOverwrite=True
 }
